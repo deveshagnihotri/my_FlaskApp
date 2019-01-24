e37cdba8f65364a78744dc2118654a88fefbc41a
Devesh Agnihotri <djdevesh524@gmail.com>
2019-01-24 19:23:41 +0530

    my_FlaskApp

---
+++ data.py
@@ -0,0 +1,18 @@
+def Articles():
+    articles = [
+        {
+            'id': 1,
+            'title':'Article One',
+            'body':'this is my first article of MyFlaskApp',
+            'author':'Devesh Agnihotri',
+            'create_date':'04-08-2019'
+        },
+        {
+            'id': 2,
+            'title':'Article two',
+            'body':'this is my second article of MyFlaskApp',
+            'author':'Dev Agnihotri',
+            'create_date':'05-08-2019'
+        }
+    ]
+    return articles
